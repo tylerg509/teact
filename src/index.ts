15f20a82388b055e329fa66c7f77d1090018de4d
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-02 21:47:06 -0800

    add trigger function

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,16 @@ const user = new User({name: 'myname', age:20})
 user.set({name: 'newName', age: 999})
 user.set({name: 'Red'})
 
-user.on('change', () => {})
-user.on('tylerRocks', ()=>{})
+user.on('change', () => {console.log('change 1')})
+ 
+user.on('tylerRocks', ()=>{console.log('change2')})
 
-console.log(user.get('name'));
-console.log(user.get('age'))
-console.log(user)
+user.on('save', ()=>{
+    console.log('save was triggered')
+})
+
+
+user.trigger('change')
+user.trigger('tylerRocks')
+user.trigger('save')
+user.trigger('aslffasdoiu')
