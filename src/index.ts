caae54ea28c002c31722fc64752e6d3dff039e4e
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-02 20:47:06 -0800

    add ability to register events

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const user = new User({name: 'myname', age:20})
 user.set({name: 'newName', age: 999})
 user.set({name: 'Red'})
 
+user.on('change', () => {})
+user.on('tylerRocks', ()=>{})
 
 console.log(user.get('name'));
 console.log(user.get('age'))
+console.log(user)
--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,23 +1,30 @@
-interface UserProps{
-    name?: string;
-    age?: number
+interface UserProps {
+  name?: string;
+  age?: number;
 }
 
-type Callback = () => {}
+//since this returns nothing use void instead of an empyty object
+type Callback = () => void;
 
-export class User{
-    constructor(private data: UserProps){}
+export class User {
+    //we don't know what they key is so we are just using 'key' as the key of type string
+  events: { [key: string]: Callback[] } = {};
+  constructor(private data: UserProps) {}
 
-    get(propName: string): (number | string){
-        return this.data[propName]
-    }
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
 
-    set(update: UserProps):void{
-        //this.data will get overwritten with update data
-        Object.assign(this.data, update);
-    }
+  set(update: UserProps): void {
+    //this.data will get overwritten with update data
+    Object.assign(this.data, update);
+  }
 
-    on(eventName: string, callback: Callback){
-        
-    }
+  on(eventName: string, callback: Callback): void {
+    //this.events[eventName] //callback[] or undefined
+    const handlers = this.events[eventName] || []
+    handlers.push(callback)
+    this.events[eventName] = handlers;
+  }
+  
 }
