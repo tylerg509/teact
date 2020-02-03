3e9bcee46ce8ab1af5604c2d94421932354ffd23
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-03 06:28:15 -0800

    add inteface to sync generic type

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,13 +1,18 @@
 import axios, { AxiosResponse, AxiosPromise } from 'axios';
-import { UserProps, User } from './Users';
+import { User } from './Users';
 
-export class Sync {
+//without this interface typescript does not know that data: T will contain a property of id
+interface HasId{
+    id: number;
+}
+
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
     if (id) {
       //put
