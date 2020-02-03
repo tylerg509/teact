e4363687fde9bc9c8d9d3a33580669a5636eabea
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-03 05:40:48 -0800

    run test

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/Users';
 
 const user = new User({name: 'new record name', age: 900});
 
-user.save()
+user.events.on('change',() =>{
+    console.log('change!');
+})
+
+user.events.trigger('change')
